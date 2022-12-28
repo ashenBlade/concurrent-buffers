00a4fb5a5692afd002ae11b8d9f493f1abf24a54
Sergey Solovev <SVaSolovev@homecredit.ru>
2022-12-28 19:45:13 +0300

    Add interface represents buffer

---
+++ src/ConcurrentBuffers.Core/IBuffer.cs
@@ -0,0 +1,27 @@
+﻿namespace ConcurrentBuffers.Core;
+
+/// <summary>
+/// Interface, that represents buffer with ability to insert and flush
+/// </summary>
+/// <typeparam name="T">Item type stored in buffer</typeparam>
+public interface IBuffer<T>
+{
+    /// <summary>
+    /// Add item to buffer
+    /// </summary>
+    /// <param name="item">Item to add</param>
+    void Add(T item);
+
+    /// <summary>
+    /// Add multiple items to buffer
+    /// </summary>
+    /// <param name="items">Items to add</param>
+    void AddRange(IEnumerable<T> items);
+
+    /// <summary>
+    /// Get all items in buffer
+    /// </summary>
+    /// <returns>Items stored in buffer</returns>
+    /// <remarks>After call, stored items are deleted from queue</remarks>
+    IEnumerable<T> Flush();
+}
