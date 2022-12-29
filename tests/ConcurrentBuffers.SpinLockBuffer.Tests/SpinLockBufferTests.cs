e5f3380bc779bf65bfe72fbeea4f463d5b046402
Sergey Solovev <SVaSolovev@homecredit.ru>
2022-12-29 07:09:34 +0300

    Add tests for concurrent flush and add calls for SpinLockBuffer

--- tests/ConcurrentBuffers.SpinLockBuffer.Tests/SpinLockBufferTests.cs
+++ tests/ConcurrentBuffers.SpinLockBuffer.Tests/SpinLockBufferTests.cs
@@ -1,11 +1,19 @@
+using System.Collections.Concurrent;
 using Xunit;
+using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace ConcurrentBuffers.SpinLockBuffer.Tests;
 
 public class UnitTest1
 {
+    private readonly ITestOutputHelper _testOutputHelper;
     private SpinLockBuffer<int> _buffer = new();
+
+    public UnitTest1(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
 
     [Theory]
     [InlineData(1)]
@@ -77,14 +85,86 @@ public class UnitTest1
         Assert.Equal(expected, actual);
     }
 
+    public static IEnumerable<object[]> ManyRandomInts =>
+        new[]
+        {
+            new object[] {GenerateRandomInts(100)}, new object[] {GenerateRandomInts(200)},
+            new object[] {GenerateRandomInts(300)}, new object[] {GenerateRandomInts(500)},
+            new object[] {GenerateRandomInts(1000)}, new object[] {GenerateRandomInts(2000)},
+            new object[] {GenerateRandomInts(5000)},
+        };
+
     [Theory]
-    [MemberData(nameof(RandomInts))]
+    [MemberData(nameof(ManyRandomInts))]
     public async Task AddRange_WithConcurrentCalls_ShouldAddAllElements(IEnumerable<int> values)
     {
         var expected = values.ToHashSet();
         await Task.WhenAll(expected.Chunk(5)
                                    .Select(r => Task.Run(() => _buffer.AddRange(r))));
         var actual = _buffer.Flush()
+                            .ToHashSet();
+        Assert.Equal(expected, actual);
+    }
+
+    public static IEnumerable<object[]> ManyRandomValuesAndConcurrentFlushCallsCount =>
+        new[]
+        {
+            new object[] {GenerateRandomInts(100), 2},
+            new object[] {GenerateRandomInts(200), 10},
+            new object[] {GenerateRandomInts(300), 300}, 
+            new object[] {GenerateRandomInts(500), 60},
+            new object[] {GenerateRandomInts(1000), 20},
+            new object[] {GenerateRandomInts(2000), 11},
+            new object[] {GenerateRandomInts(5000), 56},
+            new object[] {GenerateRandomInts(1000), 765},
+            new object[] {GenerateRandomInts(1000), 465},
+        };
+
+    public static IEnumerable<int> NumbersCount => 
+        new[] {100, 150, 200, 250, 300, 500, 1000, 1500, 3000, 5000, 10000};
+
+    public static IEnumerable<int> ParallelCallsCount =>
+        new[] { 2, 3, 4, 5, 10, 15, 20, 25, 30, 50, 100, 120, 150, 200, 300 };
+
+    public static IEnumerable<object[]> NumbersCountParallelCallsCount =>
+        NumbersCount
+           .SelectMany(nc => ParallelCallsCount
+                          .Select(pcc => new object[] {nc, pcc}));
+
+    [Theory]
+    [MemberData(nameof(NumbersCountParallelCallsCount))]
+    public void Flush_WithConcurrentAddCalls_ShouldFlushAllAddedElements(int numbersCount, int concurrentFlushCalls)
+    {
+        var expected = GenerateRandomInts(numbersCount).ToHashSet();
+        var flushed = new ConcurrentQueue<IEnumerable<int>>();
+        const int maxWaitTime = 1000;
+        Task.WaitAll(expected
+                    .Chunk(10)
+                    .Select((chunk, i) => Task.Run(async () =>
+                     {
+                         await Task.Delay(Random.Shared.Next(0, maxWaitTime));
+                         if (Random.Shared.Next(0, 1) == 0)
+                         {
+                             foreach (var number in chunk)
+                             {
+                                 _buffer.Add(number);
+                             }
+                         }
+                         else
+                         {
+                             _buffer.AddRange(chunk);
+                         }
+                     }))
+                    .Concat(Enumerable.Range(0, concurrentFlushCalls)
+                                      .Select(i => Task.Run(async () =>
+                                       {
+                                           await Task.Delay(Random.Shared.Next(0, maxWaitTime));
+                                           flushed.Enqueue(_buffer.Flush());
+                                       })))
+                    .ToArray());
+        flushed.Enqueue(_buffer.Flush());
+        var actual = flushed.ToArray()
+                            .SelectMany(i => i)
                             .ToHashSet();
         Assert.Equal(expected, actual);
     }
