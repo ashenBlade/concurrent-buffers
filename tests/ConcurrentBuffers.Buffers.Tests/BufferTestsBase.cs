b6a76946af8265b6940b12433a9bc5e3db1ed7d8
Sergey Solovev <SVaSolovev@homecredit.ru>
2022-12-29 20:15:47 +0300

    Extract base test classes

---
+++ tests/ConcurrentBuffers.Buffers.Tests/BufferTestsBase.cs
@@ -0,0 +1,66 @@
+﻿using ConcurrentBuffers.Abstractions;
+using Xunit;
+
+namespace ConcurrentBuffers.Buffers.Tests;
+
+public abstract class BufferTestsBase<TBuffer> where TBuffer: IBuffer<int>
+{
+    public abstract TBuffer Buffer { get; }
+    
+    [Theory(DisplayName = "Fulsh")]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(10)]
+    public void FlushAfterAdd_WithSingleElement_ShouldReturnAddedElement(int expected)
+    {
+        Buffer.Add(expected);
+        var flushed = Buffer
+                     .Flush()
+                     .ToArray();
+        
+        Assert.Single(flushed);
+        var actual = flushed.First();
+        Assert.Equal(expected, actual);
+    }
+
+    public static IEnumerable<int> GenerateRandomNumbers(int amount) => 
+        Enumerable
+           .Repeat(0, amount)
+           .Select(_ => Random.Shared.Next());
+
+    public static IEnumerable<object[]> RandomNumbers =>
+        new[]
+        {
+            new[] {GenerateRandomNumbers(1)}, 
+            new[] {GenerateRandomNumbers(2)}, 
+            new[] {GenerateRandomNumbers(3)}, 
+            new[] {GenerateRandomNumbers(4)}, 
+            new[] {GenerateRandomNumbers(5)}, 
+            new[] {GenerateRandomNumbers(10)}, 
+            new[] {GenerateRandomNumbers(20)}, 
+            new[] {GenerateRandomNumbers(30)}, 
+            new[] {GenerateRandomNumbers(50)}, 
+        };
+
+    [Theory]
+    [MemberData(nameof(RandomNumbers))]
+    public void FlushAfterAddRange_WithMultipleElements_ShouldReturnAllAddedElements(IEnumerable<int> values)
+    {
+        var expected = values.ToArray();
+        Buffer.AddRange(expected);
+        var actual = Buffer.Flush();
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [MemberData(nameof(RandomNumbers))]
+    public void FlushAfterFlush_ShouldReturnNoElements(IEnumerable<int> values)
+    {
+        Buffer.AddRange(values);
+        Buffer.Flush();
+        var actual = Buffer.Flush();
+        Assert.Empty(actual);
+    }
+}
---
+++ tests/ConcurrentBuffers.Buffers.Tests/ConcurrentBufferTestsBase.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Concurrent;
+using ConcurrentBuffers.Abstractions;
+using Xunit;
+
+namespace ConcurrentBuffers.Buffers.Tests;
+
+public abstract class ConcurrentBufferTestsBase<TConcurrentBuffer>
+    : BufferTestsBase<TConcurrentBuffer> 
+    where TConcurrentBuffer: IConcurrentBuffer<int>
+{
+    public override TConcurrentBuffer Buffer { get; }
+
+    [Theory]
+    [MemberData(nameof(RandomNumbers))]
+    public async Task Add_WithConcurrentCalls_ShouldAddAllElements(IEnumerable<int> values)
+    {
+        var expected = values.ToHashSet();
+        await Task.WhenAll(expected
+                          .Select(v => Task.Run(() => Buffer.Add(v)))
+                          .ToArray());
+        var actual = Buffer.Flush()
+                            .ToHashSet();
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(200)]
+    [InlineData(300)]
+    [InlineData(500)]
+    [InlineData(1000)]
+    [InlineData(2000)]
+    [InlineData(5000)]
+    public async Task AddRange_WithConcurrentCalls_ShouldAddAllElements(int numbersCount)
+    {
+        var expected = GenerateRandomNumbers(numbersCount).ToHashSet();
+        await Task.WhenAll(expected.Chunk(5)
+                                   .Select(r => Task.Run(() => Buffer.AddRange(r))));
+        var actual = Buffer.Flush()
+                            .ToHashSet();
+        Assert.Equal(expected, actual);
+    }
+
+    public static IEnumerable<int> NumbersCount => 
+        new[] {100, 150, 200, 250, 300, 500, 1000, 1500, 3000, 5000, 10000};
+
+    public static IEnumerable<int> ParallelCallsCount =>
+        new[] { 2, 3, 4, 5, 10, 15, 20, 25, 30, 50, 100, 120, 150, 200, 300 };
+
+    public static IEnumerable<object[]> NumbersCountParallelCallsCount =>
+        NumbersCount
+           .SelectMany(nc => ParallelCallsCount
+                          .Select(pcc => new object[] {nc, pcc}));
+
+    const int MaxWaitTime = 1000;
+    
+    // Timeout for deadlock check
+    [Theory(Timeout = MaxWaitTime * 10)]
+    [MemberData(nameof(NumbersCountParallelCallsCount))]
+    public void Flush_WithConcurrentAddCalls_ShouldFlushAllAddedElements(int numbersCount, int concurrentFlushCalls)
+    {
+        var expected = GenerateRandomNumbers(numbersCount).ToHashSet();
+        var flushed = new ConcurrentQueue<IEnumerable<int>>();
+        Task.WaitAll(expected
+                    .Chunk(10)
+                    .Select((chunk, i) => Task.Run(async () =>
+                     {
+                         await Task.Delay(Random.Shared.Next(0, MaxWaitTime));
+                         if (Random.Shared.Next(0, 1) == 0)
+                         {
+                             foreach (var number in chunk)
+                             {
+                                 Buffer.Add(number);
+                             }
+                         }
+                         else
+                         {
+                             Buffer.AddRange(chunk);
+                         }
+                     }))
+                    .Concat(Enumerable.Range(0, concurrentFlushCalls)
+                                      .Select(i => Task.Run(async () =>
+                                       {
+                                           await Task.Delay(Random.Shared.Next(0, MaxWaitTime));
+                                           flushed.Enqueue(Buffer.Flush());
+                                       })))
+                    .ToArray());
+        flushed.Enqueue(Buffer.Flush());
+        var actual = flushed.ToArray()
+                            .SelectMany(i => i)
+                            .ToHashSet();
+        Assert.Equal(expected, actual);
+    }
+}
